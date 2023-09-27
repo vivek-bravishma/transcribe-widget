e174ed91c93f05cf312d5f2c69d75567cc418757
ViveK <vivekn@bravishma.com>
2023-09-27 17:51:55 +0530

    chat mapping changed

--- src/components/Chat/index.jsx
+++ src/components/Chat/index.jsx
@@ -3,64 +3,67 @@ import io from "socket.io-client";
 import "./style.css";
 
 function Chat() {
-  const [messages, setMessages] = useState([]);
-  const [newMessage, setNewMessage] = useState("");
+	const [messages, setMessages] = useState([]);
+	const [newMessage, setNewMessage] = useState("");
 
-  const handleSendMessage = (e) => {
-    e.preventDefault();
-    if (newMessage.trim() === "") return;
+	const handleSendMessage = (e) => {
+		e.preventDefault();
+		if (newMessage.trim() === "") return;
 
-    const updatedMessages = [...messages, { text: newMessage, sender: "You" }];
-    setMessages(updatedMessages);
+		const updatedMessages = [
+			...messages,
+			{ text: newMessage, sender: "You" },
+		];
+		setMessages(updatedMessages);
 
-    setNewMessage("");
-  };
+		setNewMessage("");
+	};
 
-  useEffect(() => {
-    const socket = io("http://localhost:3000");
+	useEffect(() => {
+		const socket = io("http://localhost:3000");
 
-    socket.on("data", (data) => {
-      console.log("Received data from server:", data);
+		socket.on("data", (data) => {
+			console.log("Received data from server:", data);
 
-      const text = data.details.transcriptionResults[0]?.text;
-      console.log("text--> ", text);
+			// const text = data.details.transcriptionResults[0]?.text;
+			const { original_text: text, speaker: sender } = data;
+			console.log("text--> ", text);
 
-      setMessages((prev) => [...prev, { text, sender: "server" }]);
-    });
+			setMessages((prev) => [...prev, { text, sender }]);
+		});
 
-    return () => {
-      socket.disconnect();
-    };
-  }, []);
+		return () => {
+			socket.disconnect();
+		};
+	}, []);
 
-  return (
-    <div className="chat-container">
-      <div className="chat-messages">
-        {messages.map((message, index) => (
-          <div
-            key={index}
-            className={`chat-message ${
-              message.sender === "You" ? "you" : "other"
-            }`}
-          >
-            <span className="sender">{message.sender}</span>
-            {" : "}
-            <span> {message.text} </span>
-          </div>
-        ))}
-      </div>
-      <form className="chat-input" onSubmit={handleSendMessage}>
-        <input
-          className="chat-box"
-          type="text"
-          placeholder="Type your message..."
-          value={newMessage}
-          onChange={(e) => setNewMessage(e.target.value)}
-        />
-        <button className="chat-btn">Send</button>
-      </form>
-    </div>
-  );
+	return (
+		<div className="chat-container">
+			<div className="chat-messages">
+				{messages.map((message, index) => (
+					<div
+						key={index}
+						className={`chat-message ${
+							message.sender === "You" ? "you" : "other"
+						}`}>
+						<span className="sender">{message.sender}</span>
+						{" : "}
+						<span> {message.text} </span>
+					</div>
+				))}
+			</div>
+			<form className="chat-input" onSubmit={handleSendMessage}>
+				<input
+					className="chat-box"
+					type="text"
+					placeholder="Type your message..."
+					value={newMessage}
+					onChange={(e) => setNewMessage(e.target.value)}
+				/>
+				<button className="chat-btn">Send</button>
+			</form>
+		</div>
+	);
 }
 
 export default Chat;
