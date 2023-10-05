dee14b87e40caed3f75e39230e6d04fcbf085767
ViveK <vivekn@bravishma.com>
2023-10-05 18:26:45 +0530

    webpack config changed

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,37 @@
 const path = require("path");
 
 module.exports = {
-  entry: path.resolve(__dirname, "src/Widget", "Widget.tsx"),
-  output: {
-    path: path.resolve(__dirname, "build/dist"),
-    // path: path.resolve(__dirname, "build/elements"),
-    filename: "Widget.js",
-    // filename: "angular-widget-userdetails-exchangerate.js",
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: "ts-loader",
-        options: {
-          compilerOptions: {
-            noEmit: false,
-          },
-        },
-        exclude: /node_modules/,
-        include: path.resolve(__dirname, "src"),
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
+	entry: path.resolve(__dirname, "src/Widget", "Widget.tsx"),
+	output: {
+		path: path.resolve(__dirname, "build/dist"),
+		// path: path.resolve(__dirname, "build/elements"),
+		filename: "Widget.js",
+		// filename: "angular-widget-userdetails-exchangerate.js",
+	},
+	resolve: {
+		extensions: [".ts", ".tsx", ".js"],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				loader: "ts-loader",
+				options: {
+					compilerOptions: {
+						noEmit: false,
+					},
+				},
+				exclude: /node_modules/,
+				include: path.resolve(__dirname, "src"),
+			},
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+			{
+				test: /\.(gif|svg|jpg|png)$/,
+				loader: "file-loader",
+			},
+		],
+	},
 };
